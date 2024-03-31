ee46b4df16358a4d031db87c278d59372977a079
Bhuwan Bhurtel <astrofic7@gmail.com>
2024-03-31 19:54:57 +0545

    Create Homepage/Navbar/ThemeMode

---
+++ frontend/app/(pages)/docs/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import SidebarNav from './components/SidebarNav'
+import InfoSection from './components/InfoSection'
+
+const page = () => {
+  return (
+    <div className="flex">
+      <InfoSection />
+    </div>
+  )
+}
+
+export default page
