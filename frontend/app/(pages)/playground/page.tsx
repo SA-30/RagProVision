a187107a56c7d899699644ab86ca47658c53eaf1
Bhuwan Bhurtel <astrofic7@gmail.com>
2024-03-31 23:01:13 +0545

    #RAV-6-homepage-navbar-thememode

---
+++ frontend/app/(pages)/playground/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { TooltipProvider } from '@radix-ui/react-tooltip'
+
+const page = () => {
+  return (
+    <div>
+      <h1 className='font-semibold text-2xl'>Playground</h1>
+    </div>
+  )
+}
+
+export default page
